3ad8c6f50f86ccf09c6320b145e561d4f2bc7f39
PieOned <84640491+PieOned@users.noreply.github.com>
2023-11-24 14:53:54 +0300

    Delete Parser7-main directory

--- Parser7-main/store.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class Store():
-   def __init__(self):
-      self.data = pd.Series()
-   def store(self, data):
-      print(self.data.append(data))
-   def show(self):
-      print(self.data)
